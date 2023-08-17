8827a31028c0afbc43037c022781b84555de31a9
khaledafnouch <khaledafnouch@gmail.com>
2023-08-17 18:20:53 +0100

    Add template file

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'blog/index.html')
+    
+    
+def contact(request):
+    return render(request,'blog/contact.html')
